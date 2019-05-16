6bc9e4ee33d949992bceb549ea8475431db234a4
Denzil Martin.J <denzilmart@gmail.com>
2019-05-16 19:13:36 +0530

    menu hover and active

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -6,10 +6,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
+billing : boolean = true;
+claimV : boolean = false;
+claimT : boolean = false;
+policy : boolean = false;
+broker : boolean = false;
+product : boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setActive(menu){
+    if (menu === 'billing'){
+      this.billing = true;
+      this.claimV = false;
+      this.claimT  = false;
+      this.policy = false;
+      this.broker  = false;
+      this.product  = false;  
+    }
+    else if(menu === 'claimV'){
+      this.billing = false;
+      this.claimV = true;
+      this.claimT  = false;
+      this.policy = false;
+      this.broker  = false;
+      this.product  = false;  
+    }
+     else if(menu == 'claimT'){
+      this.billing = false;
+      this.claimV = false;
+      this.claimT  = true;
+      this.policy = false;
+      this.broker  = false;
+      this.product  = false;  
+    }
+     else if(menu == 'policy'){
+      this.billing = false;
+      this.claimV = false;
+      this.claimT  = false;
+      this.policy = true;
+      this.broker  = false;
+      this.product  = false;  
+    }
+     else if(menu == 'broker'){
+      this.billing = false;
+      this.claimV = false;
+      this.claimT  = false;
+      this.policy = false;
+      this.broker  = true;
+      this.product  = false;  
+    }
+     else if(menu == 'product'){
+      this.billing = false;
+      this.claimV = false;
+      this.claimT  = false;
+      this.policy = false;
+      this.broker  = false;
+      this.product  = true;  
+    }
   }
 
 }
